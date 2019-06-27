373c2e461342ca529070cb5ecb5e85c7de8b7e92
chucnguyenvan <nguyenvanchuc12@gmail.com>
2019-06-27 10:25:22 +0700

    THUCHANHBIENVAKIEUDULIEU

---
+++ sudungtoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth= prompt("Nhap vao chieu rong:")
+inputHeight= prompt("Nhap vao chieu dai:")
+let width =parseInt(inputWidth);
+let height =parseInt(inputHeight);
+let area =width*height;
+document.write("DT HCN la:" +area);
